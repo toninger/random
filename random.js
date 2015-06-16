fcec4205ca35fe75525f8458e4928aa2c5f0983d
Toni Coll Barber <toninger@gmail.com>
2015-06-16 15:34:42 +0200

    random version 2

--- random.js
+++ random.js
@@ -1,7 +1,7 @@
 var numero = Math.random();
 
 if (numero <= 0.5) {
-	console.log('\n' + numero + 'MENOR 	que 0,5 \n');
+	console.log('\n' + numero + ' es MENOR 	que 0,5 \n');
 } else {
-	console.log('\n' + numero + 'MAYOR 	que 0,5 \n');
+	console.log('\n' + numero + ' es MAYOR 	que 0,5 \n');
 }
